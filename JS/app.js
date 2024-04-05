bf7b858526d651d63a90e3ca43facee8e4eee16d
Urbani Michele <michele.urbani094@gmail.com>
2024-04-05 22:48:30 +0200

    ADD: Milestone 1 da finire

--- JS/app.js
+++ JS/app.js
@@ -2,53 +2,100 @@ console.log('MILESTONE 1');
 
 // Al click dell'utente su "Calcola preventivo" il sito stampa il prezzo finale
 
-// Ore richieste per il progetto
-
-const lavoriElement = document.getElementById("lavori");
-console.log(lavoriElement);
+// const corsi = [
+//     {
+//       nome: "Backend",
+//       prezzo: 20.50,
+//     },
+//     {
+//       nome: "Frontend",
+//       prezzo: 15.30,
+//     },
+//     {
+//       nome: "Analysis",
+//       prezzo: 33.60,
+//     },
+//   ];
+// console.log(corsi);
 
 const ore = 10;  // Number
 
-
-
 // Prendo dal DOM gli elementi che mi servono per gestire il form
-const buttonElement = document.getElementById("bottone");
-console.log(buttonElement);
 
 const formElement = document.getElementById("form-commissioni");
-console.log(formElement);
+// console.log(formElement);
 
-
-
-// Chiedere all'utente la commissione da eseguire
-
-
+const lavoriElement = document.getElementById("lavori");
+// console.log(lavoriElement);
 
 const sconti = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
-console.log(sconti);
+// console.log(sconti);
 
-// Calcolare il preventivo finale che sarà = prezzo base - sconto
+const buttonElement = document.getElementById("bottone");
+// console.log(buttonElement);
+
+const risultatoElement = document.getElementById('risultato');
+// console.log(risultatoElement);
 
 formElement.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // SE utente 1a
-    // ALTRIMENTI SE 2a
-    // ALTRIMENTI 3a
-    
+    const form = formElement.value;
+    // console.log(form);
+
+    // Raccolgo il value delle options dalla select
+    const lavori = document.getElementById("lavori").value;
+    // console.log(lavori);
+    let prezzoOra = 0;
+    // SE utente seleziona value '1' --> 20.50
+    // ALTRIMENTI SE seleziona value '2' --> 15.30
+    // ALTRIMENTI SE seleziona value '3' --> 33.60
+    if (lavori.length === '1') {
+        prezzoOra = 20.50;
+    } else if (lavori === '2') {
+        prezzoOra = 15.30;
+    } else if (lavori === '3') {
+        prezzoOra = 33.60;
+    }
+
+    console.log(prezzoOra);
+
+
     // Calcolo il prezzo base in base alle ore
     // costoOrario * ore
     
+    prezzoBase = prezzoOra * ore;
+    console.log(prezzoBase);
+
     // SE il codice inserito è valido 
     // - ALLORA applico lo sconto
     // ALTRIMENTI 
     // - avvisiamo che il codice non è valido
     
+
+    for (let i = 0; i < sconti.length; i++) {
+        const codice = sconti[i];
+        // console.log(codice);
+        
+
+    
+    if (codice.length === 7 && /^[A-A0-9]+$/.test(codice)) {
+        
+        const lunghezzaCodice = codice.length
+        
+        console.log(`Il codice ${codice} è lungo ${lunghezzaCodice}`);
+
+    } else {
+    // console.log('Codice non Valido');
+    }}
+
     // Stampo il prezzo nella pagina
+    // Utilizzo il risultato del prezzo in base al tipo di lavoro selezionato
+    risultatoElement.innerHTML = 'Prezzo Finale';
 
     // const lavori = lavoriElement.value;
 
 
 
-    console.log('submit', lavori);
+    // console.log('submit', lavori);
 })
