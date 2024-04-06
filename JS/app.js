a7f3fbf3c258cff24aa95182d58afaa2e5673927
Urbani Michele <michele.urbani094@gmail.com>
2024-04-06 16:22:06 +0200

    ADD: HTML is-invalid, JS sistemato il codice con prezzo finale, da sistemare il codice sconto

--- JS/app.js
+++ JS/app.js
@@ -23,35 +23,30 @@ const ore = 10;  // Number
 // Prendo dal DOM gli elementi che mi servono per gestire il form
 
 const formElement = document.getElementById("form-commissioni");
-// console.log(formElement);
 
 const lavoriElement = document.getElementById("lavori");
-// console.log(lavoriElement);
 
 const sconti = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
-// console.log(sconti);
 
-// const codicePromo = document.getElementById('codicepromo');
-// console.log(codicePromo);
+const codicePromo = document.getElementById('codicepromo');
 
 const buttonElement = document.getElementById("bottone");
-// console.log(buttonElement);
 
 const risultatoElement = document.getElementById('risultato');
-// console.log(risultatoElement);
+
+// Utilizzo un form e lo gestisco applicando la funzione evento submit
 
 formElement.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // const form = formElement.value;
-    // // console.log(form);
-    // function name(params) {
-        
-    // }
     // Raccolgo il value delle options dalla select
     const lavori = document.getElementById("lavori").value;
     // console.log(lavori);
+
+    // Assegno il prezzoOra ad una variabile con valore 0
     let prezzoOra = 0;
+
+    // Aggiungo i value ai prezzi * ore
     // SE utente seleziona value '1' --> 20.50
     // ALTRIMENTI SE seleziona value '2' --> 15.30
     // ALTRIMENTI SE seleziona value '3' --> 33.60
@@ -63,39 +58,37 @@ formElement.addEventListener('submit', function (event) {
         prezzoOra = 33.60;
     }
 
+    // Stampo prezzoOra
     console.log(prezzoOra);
 
+    // Calcolo il prezzo base in base alle ore e lo assegno ad una variabile
+    let prezzoBase = prezzoOra * ore; // Number
 
-    // Calcolo il prezzo base in base alle ore
-    // costoOrario * ore
-    
-    prezzoBase = prezzoOra * ore;
+    // Stampo prezzoBase
     console.log(prezzoBase);
 
+    // Inserisco Validatore Sconto
     // SE il codice inserito è valido 
     // - ALLORA applico lo sconto
     // ALTRIMENTI 
     // - avvisiamo che il codice non è valido
     
-    for (let i = 0; i < sconti.length; i++) {
-        console.log(sconti[i]);
+    // Prendo il value dal DOM e lo assegno ad una variabile
+    let sconto = codicePromo.value;
 
-        const codicePromo = document.getElementById('codicepromo');
-        const promo = codicePromo.value
-        if (sconti.includes(promo)) {
-            console.log('Codice valido');
-        } else {
-            console.log('Codice non valido');
-        }
+    if (sconti.includes(sconto)) {
+        sconto = 0.25;
+    } else {
+        
     }
 
+    const scontoApplicato = prezzoBase * sconto; // Number
+
+    const prezzoFinale = prezzoBase - scontoApplicato;
+    console.log(prezzoFinale);
     // Stampo il prezzo nella pagina
     // Utilizzo il risultato del prezzo in base al tipo di lavoro selezionato
-    risultatoElement.innerHTML = 'Prezzo Finale';
+    risultatoElement.innerHTML = `${prezzoFinale.toFixed(2)} &euro;`;
+    console.log(prezzoFinale);
 
-    // const lavori = lavoriElement.value;
-
-
-
-    // console.log('submit', lavori);
 })
